8db2427a912dc8cff8ab3b085e931bb0bb4e6827
Anthony Jung <junganthonyy@gmail.com>
2020-02-13 18:15:09 -0800

    Converted to work with tsv's

--- database/faketripwrite.js
+++ database/faketripwrite.js
@@ -3,23 +3,23 @@ const fs = require('fs');
 
 const start = Date.now();
 
-var stream = fs.createWriteStream('./SeedData/sampleData.json', 'utf-8');
+var stream = fs.createWriteStream('./database/SeedData/sampleData.tsv', 'utf-8');
 
 function write10MillTrips(writer, encoding, callback) {
   let i = 0;
-  const numTrips = 10000000;
+  const numTrips = 10;
 
-  writer.write('[', encoding);
+  // write the header here for the csv
+  // 
+  writer.write('id\tname\tlocation\tdays\tcities\tmapPic\tschedule\toptionals\tcoordinates\n', encoding);
 
   write();
   function write() {
     let ok = true;
 
-    
-
     do {
       i++;
-      let data = JSON.stringify(fakeTrip(i), null, 2);
+      let data = fakeTrip(i);
 
       if (i === numTrips) {
         // Last time!
@@ -27,7 +27,7 @@ function write10MillTrips(writer, encoding, callback) {
       } else {
         // See if we should continue, or wait.
         // Don't pass the callback, because we're not done yet.
-        data += ',\n';
+        data += '\n';
         ok = writer.write(data, encoding);
       }
     } while (i < numTrips && ok);
@@ -46,3 +46,46 @@ write10MillTrips(stream, 'utf-8', () => {
 
   console.log((Date.now() - start)/1000);
 });
+
+
+
+// Code for JSON file
+// function write10MillTrips(writer, encoding, callback) {
+//   let i = 0;
+//   const numTrips = 10;
+
+//   writer.write('[', encoding);
+
+//   write();
+//   function write() {
+//     let ok = true;
+
+//     do {
+//       i++;
+//       let data = JSON.stringify(fakeTrip(i), null, 2);
+
+//       if (i === numTrips) {
+//         // Last time!
+//         writer.write(data, encoding, callback);
+//       } else {
+//         // See if we should continue, or wait.
+//         // Don't pass the callback, because we're not done yet.
+//         data += ',\n';
+//         ok = writer.write(data, encoding);
+//       }
+//     } while (i < numTrips && ok);
+
+//     if (i < numTrips) {
+//       // Had to stop early!
+//       // Write some more once it drains.
+//       writer.once('drain', write);
+//     }
+//   }
+// }
+
+// write10MillTrips(stream, 'utf-8', () => {
+//   stream.write(']', 'utf-8')
+//   stream.end();
+
+//   console.log((Date.now() - start)/1000);
+// });
